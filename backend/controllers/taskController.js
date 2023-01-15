2f2bb0d1b507853ed05961b489b1db65ca02825c
Henry Fan <henry.fan2313@gmail.com>
2023-01-14 19:47:04 -0500

    Started implementation for GET, PUT, POST, DELETE

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,34 @@
+// @description - gets all tasks of a user
+// @route - GET /api/goals
+// @access - Private
+const getTasks = (req, res) => {
+    res.status(200).json({message: "Get tasks"})
+}
+
+// @description - sets a task of a user
+// @route - GET /api/goals
+// @access - Private
+const setTask = (req, res) => {
+    res.status(200).json({message: "Set task"})
+}
+
+// @description - Update a task of a user
+// @route - PUT /api/goals/:id
+// @access - Private
+const updateTask = (req, res) => {
+    res.status(200).json({message: `Update task ${req.params.id}`})
+}
+
+// @description - Delete a task of a user
+// @route - DELETE /api/goals/:id
+// @access - Private
+const deleteTask = (req, res) => {
+    res.status(200).json({message: `Delete task ${req.params.id}`})
+}
+
+module.exports = {
+    getTasks,
+    setTask,
+    updateTask,
+    deleteTask
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/tasks', require('./routes/taskRoutes'))
+
+app.listen(port, () => console.log(`Sever has started on port ${port}`))
